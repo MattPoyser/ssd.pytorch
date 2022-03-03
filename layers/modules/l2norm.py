99588e7b4c7dab8df14c74432e29cf6861bc2ded
MattPoyser <hedgy03@hotmail.co.uk>
2022-03-03 22:42:41 +0000

    l2norm fix

--- layers/modules/l2norm.py
+++ layers/modules/l2norm.py
@@ -17,7 +17,8 @@ class L2Norm(nn.Module):
         init.constant_(self.weight,self.gamma)
 
     def forward(self, x):
-        norm = x.pow(2).sum(dim=1, keepdim=True).sqrt()+self.eps
+        # norm = x.pow(2).sum(dim=1, keepdim=True).sqrt()+self.eps
+        norm = (x.pow(2).sum(dim=1, keepdim=True)+self.eps).sqrt() # https://github.com/amdegroot/ssd.pytorch/issues/419
         #x /= norm
         x = torch.div(x,norm)
         out = self.weight.unsqueeze(0).unsqueeze(2).unsqueeze(3).expand_as(x) * x
