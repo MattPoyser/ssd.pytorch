c0746dc60daeba0c03730f180014ce36201b774f
Matt <matthew.poyser@durham.ac.uk>
2022-03-10 16:06:49 +0000

    eps in encode for explosion prevention

--- layers/box_utils.py
+++ layers/box_utils.py
@@ -131,7 +131,8 @@ def encode(matched, priors, variances):
     g_cxcy /= (variances[0] * priors[:, 2:])
     # match wh / prior wh
     g_wh = (matched[:, 2:] - matched[:, :2]) / priors[:, 2:]
-    g_wh = torch.log(g_wh) / variances[1]
+    eps = 1e-5
+    g_wh = torch.log(g_wh+eps) / variances[1]
     # return target for smooth_l1_loss
     return torch.cat([g_cxcy, g_wh], 1)  # [num_priors,4]
 
