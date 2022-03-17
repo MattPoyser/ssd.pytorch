87832a31cd3ce10f47aeedcc744606b8b1801daf
MattPoyser <hedgy03@hotmail.co.uk>
2022-03-17 11:56:48 +0000

    eval no grad fix

--- eval.py
+++ eval.py
@@ -433,6 +433,7 @@ if __name__ == '__main__':
         net = net.cuda()
         cudnn.benchmark = True
     # evaluation
-    test_net(args.save_folder, net, args.cuda, dataset,
-             BaseTransform(net.size, dataset_mean), args.top_k, 300,
-             thresh=args.confidence_threshold)
+    with torch.no_grad():
+        test_net(args.save_folder, net, args.cuda, dataset,
+                 BaseTransform(net.size, dataset_mean), args.top_k, 300,
+                 thresh=args.confidence_threshold)
